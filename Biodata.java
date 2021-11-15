f09bd88a882dea2e06735be5bd4e9a85794d239d
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-11-15 19:30:04 +0700

    update

---
+++ Biodata.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Biodata {
+
+  
+    public static void main(String[] args) {
+       
+       System.out.println("================================");
+       System.out.println("Nama     : Yoga Meleniawan Pamungkas");
+       System.out.println("NIM      : 1931710083");
+       System.out.println("Alamat   : Malang");
+       System.out.println("================================");
+       
+    }
+    
+}
